29ee202acba91fe9d630d0408015ca0571e39d93
D-Aldin <dobricaldin@gmail.com>
2025-07-06 18:58:38 +0200

    update git ignore

---
+++ src/enviroment/enviroment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD8TnyIs3qowBP0a0aSoTUaOfKbREZlcwY',
+    authDomain: 'clientsphere-c9500.firebaseapp.com',
+    projectId: 'clientsphere-c9500',
+    storageBucket: 'clientsphere-c9500.firebasestorage.app',
+    messagingSenderId: '909497080452',
+    appId: '1:909497080452:web:dc5d80d93d95a7bb2240d1',
+  },
+};
